dedc0988b445e7d6a80a999fa5c09a5903fe7fa6
Elvira <elvira.siegel@codecentric.de>
2021-04-11 15:21:09 +0200

    make relations between different nodes in diff languages (EN, DE, FR, ES)

--- experiments/relations.py
+++ experiments/relations.py
@@ -1,9 +1,14 @@
-import json
-import urllib.request
 import sys
+import json
+import itertools
+import urllib.request
+
 import networkx as nx
 import matplotlib.pyplot as plt
-import itertools
+
+
+# give the data directory with json files in it
+data_dir = sys.argv[2]
 
 # lemma=page --> lemma=word
 # first argument "word" 
@@ -11,98 +16,90 @@ input_word = sys.argv[1]
 print("*"*10)
 print("initial word: ", "> ", input_word, " <")
 print("*"*10)
-
 # TODO: expand / change "searchLang=EN"
-# Comment out if limt reached:
-url = "https://babelnet.io/v6/getSynsetIds?lemma=[word]&searchLang=EN&key=299a9265-9746-4b40-a13f-b1cb0a7d3f1d"
-url = url.replace("[word]", input_word)
-web_get_synset_ids = urllib.request.urlopen(url)
-data = json.load(web_get_synset_ids) 
+# Comment out if limt reached ; if file with the word exists, skip:
+# url = "https://babelnet.io/v6/getSynsetIds?lemma=[word]&searchLang=EN&key=299a9265-9746-4b40-a13f-b1cb0a7d3f1d"
+# url = url.replace("[word]", input_word)
+# web_get_synset_ids = urllib.request.urlopen(url)
+# data = json.load(web_get_synset_ids) 
 
-# print(data)
-# print(type(data))
+# SAVE data in json if the file with the ids does not exist:
+# out_file = open(input_word + "_synset_ids.json", "w") 
+# json.dump(data, out_file, indent = 6) 
 
-# elem = id, pos, source
-word_syns_ids = []
-for elem in data:
-    word_syns_ids.append(elem["id"]) # type dict
 
-# idx = word_syns_ids[0]
-# print(idx)
-# url_for_lemmas = "https://babelnet.io/v6/getSynset?id=[synset_id]&targetLang=DE&targetLang=ES&targetLang=FR&key=299a9265-9746-4b40-a13f-b1cb0a7d3f1d"
-# url_for_lemmas = url_for_lemmas.replace("[synset_id]", idx)
-# web_get_synsets = urllib.request.urlopen(url_for_lemmas)
-# print(web_get_synsets)
-
-# import requests
-# headers = {
-#  'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-# }
+# LOADING json
 node_lst = []
-for idx in word_syns_ids:
-    # sometimes no senses is given, hence error "IndexError: list index out of range" occurs, so catching
-    try:
-        # id=bn:00091387v --> id=extracted_id from word_syns_ids
-        url_for_lemmas = "https://babelnet.io/v6/getSynset?id=[synset_id]&targetLang=DE&targetLang=ES&targetLang=FR&targetLang=EN&key=299a9265-9746-4b40-a13f-b1cb0a7d3f1d"
-        url_for_lemmas = url_for_lemmas.replace("[synset_id]", idx)
-        web_get_synsets = urllib.request.urlopen(url_for_lemmas)
-        # web_get_synsets = requests.get(url_for_lemmas)
-        # print(web_get_synsets)
-        data_synsets = json.load(web_get_synsets) 
-        node = (data_synsets["senses"][0]["properties"]["fullLemma"], data_synsets["senses"][0]["properties"]["language"])
-        node_lst.append(node)
-    except:
-        pass
+with open(data_dir + "synset_page_ids.json", "r") as synsets_id_json_file:
+    synset_ids = json.load(synsets_id_json_file)
+    for elem in synset_ids:
+        idx = elem["id"]
+#         # sometimes no senses is given, hence error "IndexError: list index out of range" occurs, so catching
+#         try:
+#             # id=bn:00091387v --> id=extracted_id
+#             url_for_lemmas = "https://babelnet.io/v6/getSynset?id=[synset_id]&targetLang=DE&targetLang=ES&targetLang=FR&targetLang=EN&key=299a9265-9746-4b40-a13f-b1cb0a7d3f1d"
+#             url_for_lemmas = url_for_lemmas.replace("[synset_id]", idx)
+#             web_get_synsets = urllib.request.urlopen(url_for_lemmas)
+#             data_synsets = json.load(web_get_synsets) 
+#             out_file = open(idx +"_"+ input_word + "_wordsynsets.json", "w") 
+#             json.dump(data_synsets, out_file, indent = 6) 
+#         except:
+#             pass
+
+        with open(data_dir + idx +"_"+ input_word + "_wordsynsets.json", "r") as word_synsets_json:
+            word_synserts = json.load(word_synsets_json)
+            node = (word_synserts["senses"][0]["properties"]["fullLemma"], word_synserts["senses"][0]["properties"]["language"])
+            node_lst.append(node)
+
+print("1st level : ", node_lst)
+
+# for idx in word_syns_ids:
+#     # sometimes no senses is given, hence error "IndexError: list index out of range" occurs, so catching
+#     try:
+#         # id=bn:00091387v --> id=extracted_id from word_syns_ids
+#         url_for_lemmas = "https://babelnet.io/v6/getSynset?id=[synset_id]&targetLang=DE&targetLang=ES&targetLang=FR&targetLang=EN&key=299a9265-9746-4b40-a13f-b1cb0a7d3f1d"
+#         url_for_lemmas = url_for_lemmas.replace("[synset_id]", idx)
+#         web_get_synsets = urllib.request.urlopen(url_for_lemmas)
+#         # web_get_synsets = requests.get(url_for_lemmas)
+#         # print(web_get_synsets)
+#         data_synsets = json.load(web_get_synsets) 
+#         node = (data_synsets["senses"][0]["properties"]["fullLemma"], data_synsets["senses"][0]["properties"]["language"])
+#         node_lst.append(node)
+#     except:
+#         pass
 
 
 G = nx.Graph()    
-##############################
-# G.add_nodes_from(node_lst)
 
-# language_node_lst_EN = []
-# language_node_lst_DE = []
-# language_node_lst_ES = []
-# language_node_lst_FR = []
-
-# for node in node_lst:
-#     language = node[1]
-#     if language == 'EN':
-#         language_node_lst_EN.append(node)
-#     if language == 'DE':
-#         language_node_lst_DE.append(node)
-#     if language == 'ES':
-#         language_node_lst_ES.append(node)
-#     if language == 'FR':
-#         language_node_lst_FR.append(node)
-
-# G.add_nodes_from(language_node_lst_EN)
-# for elem in language_node_lst_EN:
-#     G.add_edges_from([(input_word, elem)])
-##############################
-
-
+# SAVE further meanings of words in node_lst:
 for elem in node_lst:
-    G.add_edges_from([(input_word, elem)])
-    further_word = elem[0]                      # TODO
-    url = "https://babelnet.io/v6/getSenses?lemma=bank&searchLang=EN&searchLang=DE&searchLang=FR&searchLang=ES&key=299a9265-9746-4b40-a13f-b1cb0a7d3f1d"
-    url = url.replace("[word]", further_word)
-    web_get_synset_ids = urllib.request.urlopen(url)
-    further_word_data = json.load(web_get_synset_ids)
-    # print("-"*20)
-    # # print(further_word_data)
-    # next_sense_node = (further_word_data[0]["properties"]) #[0]["fullLemma"], further_word_data["properties"]["language"])
-    # print("next: ", next_sense_node)
-    # G.add_edges_from([(elem, next_elem_sense)])
+    G.add_edges_from([(input_word, elem)]) # example elem = ('Page_boy_(wedding_attendant)', 'EN')
+    further_word = elem[0]
 
+    # COMMENT OUT if files do not exist else give the files in
+    # try:
+    #     # [word] may have non ascii chars
+    #     url = "https://babelnet.io/v6/getSenses?lemma=[word]&searchLang=EN&searchLang=DE&searchLang=FR&searchLang=ES&key=299a9265-9746-4b40-a13f-b1cb0a7d3f1d"
+    #     url = url.replace("[word]", further_word)
+    #     web_get_synset_ids = urllib.request.urlopen(url)
+    #     further_word_data = json.load(web_get_synset_ids)
+    #     out_file = open(further_word + "_wordSenses.json", "w") 
+    #     json.dump(further_word_data, out_file, indent = 6) 
+    # except:
+    #     pass
 
+    try:
+        with open(data_dir + further_word + "_wordSenses.json", "r") as further_word_json:
+            further_word_senses = json.load(further_word_json)
+            try:
+                further_node = (further_word_senses[0]["properties"]["fullLemma"], further_word_senses[0]["properties"]["language"])
+                print(elem, " --> ", further_node)
+                G.add_edges_from([(elem, further_node)])
+            except:
+                pass
+    except:
+        pass
+    
 
-# print(language_node_lst_EN)
-# print()
-# combine_pairs = [(node1, node2) for node1 in test_tuple1 for node2 in test_tuple2]
-# combis_within_lan = list(itertools.combinations(language_node_lst_EN, 2)) # list of 2 tuples in a tuple: [( (),() )]
-
-# for elem in combis_within_lan:
-#     G.add_edges_from([elem])
-
-# nx.draw_networkx(G)
-# plt.show()
+nx.draw_networkx(G)
+plt.show()
